35ed4e18c8eb709e245a197f390259dae81d78ca
김영현 <0manin@douzone.com>
2023-04-21 13:34:59 +0900

    20230421

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -4,14 +4,21 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
 //        MemberService memberService = new MemberServiceImpl();
-        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // AppConfig의 환경설정을 가지고 스프링 컨테이너에 만든 빈을 넣어줘
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);// 찾을 메소드 명을 name으로
+
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
