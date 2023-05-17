6b7cc44833654623594b4f52be7be89b88791fe7
김영현 <0manin@douzone.com>
2023-05-17 17:57:02 +0900

    Http request

---
+++ springmvc1/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,27 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Slf4j    // Logger~ 선언 안 해도 돼 ㅜ
+@RestController
+public class LogTestController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest(){
+        String name = "Spring";
+
+        log.trace("trace log ={}", name);
+        log.debug("debug log ={}", name);       // 개발서버
+        log.info("info log ={}", name);         // 운영서버(default)
+        log.warn("warn log ={}", name);
+        log.error("error log ={}", name);
+
+        return "OK";
+    }
+}
